4c72bf34da8f7e0995a7615df772f018a8cd08ae
n.niagu <nyagynikita@gmail.com>
2020-06-10 22:35:29 +0200

    several languages

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,6 +2,41 @@ import React from 'react';
 import './Form.css';
 import { setAction, setPlace, setPerson, setTime } from '../../redux/actions';
 import { connect } from 'react-redux';
+import i18n from "i18next";
+import { withTranslation, initReactI18next } from "react-i18next";
+
+i18n
+    .use(initReactI18next) // passes i18n down to react-i18next
+    .init({
+        resources: {
+            en: {
+                translation: {
+                    "Title": "Title",
+                    "WhoQ": "Who are you?",
+                    "WhatQ": "What are you doing?",
+                    "WhenQ": "When are you doing this?",
+                    "WhereQ": "Where are you doing this?",
+                    "Generate": "Generate sentence"
+                }
+            },
+            cz: {
+                translation: {
+                    "Title": "Nadpis",
+                    "WhoQ": "Kdo jste?",
+                    "WhatQ": "Co děláte?",
+                    "WhenQ": "Kdy to děláte?",
+                    "WhereQ": "Kde to děláte?",
+                    "Generate": "Vytvářet větu"
+                }
+            }
+        },
+        lng: "en",
+        fallbackLng: "en",
+
+        interpolation: {
+            escapeValue: false
+        }
+    });
 
 const mapDispatchToProps = dispatch => ({
     setAction: action => dispatch(setAction(action)),
@@ -15,6 +50,7 @@ const mapStateToProps = state => ({
     time: state.setDataState.time,
     place: state.setDataState.place,
     action: state.setDataState.action,
+    lang: state.setServiceState.lang,
 })
 
 class Form extends React.Component {
@@ -22,30 +58,41 @@ class Form extends React.Component {
         super();
     }
 
+    shouldComponentUpdate(nextProps) {
+        console.log("next", nextProps)
+        if (this.props.lang != nextProps.lang) {
+            console.log('must')
+            i18n.changeLanguage(nextProps.lang)
+        }
+        return true
+    }
+
     render() {
+        const { t } = this.props;
+
         const { place, person, time, action } = this.props;
         return (
             <div id="main-form">
-                <div id="form-title">Title</div>
+                <div id="form-title">{t('Title')}</div>
 
                 <div style={{ marginTop: '25px' }} className="line-wrapper">
-                    <div>Some sample question</div>
+                    <div>{t('WhoQ')}</div>
                     <input value={person} onChange={e => this.props.setPerson(e.target.value)} />
                 </div>
                 <div style={{ marginTop: '35px' }} className="line-wrapper">
-                    <div>Some sample qestion</div>
+                    <div>{t('WhatQ')}</div>
                     <input value={action} onChange={e => this.props.setAction(e.target.value)} />
                 </div><div style={{ marginTop: '35px' }} className="line-wrapper">
-                    <div>Some sample qestion</div>
+                    <div>{t('WhenQ')}</div>
                     <input value={time} onChange={e => this.props.setTime(e.target.value)} />
                 </div>
                 <div style={{ marginTop: '35px' }} className="line-wrapper">
-                    <div>Some sample qestion</div>
+                    <div>{t('WhereQ')}</div>
                     <input value={place} onChange={e => this.props.setPlace(e.target.value)} />
                 </div>
 
                 <div style={{ display: place && person && time && action ? 'block' : 'none' }} id="submit-form">
-                    <div>Go do something</div>
+                    <div>{t('Generate')}</div>
                 </div>
 
             </div  >
@@ -53,4 +100,4 @@ class Form extends React.Component {
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Form);
+export default withTranslation()(connect(mapStateToProps, mapDispatchToProps)(Form));
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import './Header.css';
+import ReactFlagsSelect from 'react-flags-select';
+import { setLanguage } from '../../redux/actions'
+import 'react-flags-select/css/react-flags-select.css';
+import { connect } from "react-redux"
+
+const mapDispatchToProps = dispatch => ({
+    setLanguage: lang => dispatch(setLanguage(lang)),
+})
+
+const mapStateToProps = state => ({
+
+})
+
+//import css module
+
 
 class Header extends React.Component {
     constructor() {
         super()
+    }
+
+    onLangSelect = countryCode => {
+        switch (countryCode) {
+            case ("GB"):
+                this.props.setLanguage('en');
+                break;
+            case ("CZ"):
+                this.props.setLanguage('cz');
+                break;
+            default:
+                this.props.setLanguage('en');
+        }
     }
 
     render() {
@@ -12,7 +40,17 @@ class Header extends React.Component {
                 <div id="nav-wrapper">
 
                     <div className="header-button">1</div>
-                    <div className="header-button">1</div>
+                    {/* <div className="header-button">1</div> */}
+                    <div className="header-button">
+                        <ReactFlagsSelect
+                            defaultCountry="GB"
+                            countries={["GB", "CZ"]}
+                            customLabels={{ "GB": "EN", "CZ": "CZ" }}
+                            showSelectedLabel={false}
+                            selectedSize={20}
+                            onSelect={this.onLangSelect}
+                        />
+                    </div>
                     <div className="header-button">1</div>
                 </div>
 
@@ -21,4 +59,4 @@ class Header extends React.Component {
     }
 }
 
-export default Header;
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
